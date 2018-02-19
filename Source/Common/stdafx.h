528e6dcac270f58066e6bd96aafaa9d200ce684c
N3xoX1 <n3xox1@gmail.com>
2018-02-19 17:54:03 +1100

    Add common

---
+++ Source/Common/stdafx.h
@@ -0,0 +1,14 @@
+#pragma warning(disable:4786)
+
+#include <map>
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "Platform.h"
+#include "StdString.h"
+#include "CriticalSection.h"
+#include "FileClass.h"
+#include "path.h"
+#include "LogClass.h"
+#include "TraceModulesCommon.h"
+#include "Trace.h"
