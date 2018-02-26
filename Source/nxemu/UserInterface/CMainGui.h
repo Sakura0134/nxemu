d421d20f64b45a068221118f63f4d23fcd6f0e93
N3xoX1 <n3xox1@gmail.com>
2018-02-26 18:03:36 +1100

    Add WTLApp.h

--- Source/nxemu/UserInterface/CMainGui.h
+++ Source/nxemu/UserInterface/CMainGui.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "WTLApp.h"
 
 class CMainGui
 {
--- Source/nxemu/main.cpp
+++ Source/nxemu/main.cpp
@@ -2,14 +2,23 @@
 #include "UserInterface/Notification.h"
 #include <Common/path.h>
 #include <nxemu-core/AppInit.h>
-#include <windows.h>
 
-int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPSTR /*lpszArgs*/, int /*nWinMode*/)
+void RunApplication(void)
 {
     AppInit(&Notify(), CPath(CPath::MODULE_DIRECTORY), __argc, __argv);
     CMainGui MainWindow(stdstr_f("NXEmu %s", VER_FILE_VERSION_STR).ToUTF16().c_str());
     MainWindow.Show(true);	//Show the main window
+
     MainWindow.ProcessAllMessages();
     AppCleanup();
+}
+
+#include <windows.h>
+
+WTLModule _Module;
+
+int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPSTR /*lpszArgs*/, int /*nWinMode*/)
+{
+    RunApplication();
     return true;
 }
