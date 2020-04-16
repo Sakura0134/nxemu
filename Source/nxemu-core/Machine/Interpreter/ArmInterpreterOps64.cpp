646daab009d1566ee3374109cd4f9e0f9c23895a
N3xoX1 <n3xox1@gmail.com>
2020-04-16 14:39:44 +0930

    add 64bit immediate handling to Arm64Op::Add

--- Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
+++ Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
@@ -8,7 +8,11 @@ void Arm64Op::Add(CPUExecutor & core, const Arm64Opcode &op)
     if (op.Operands() == 3 && op.Operand(0).type == Arm64Opcode::ARM64_OP_REG && op.Operand(1).type == Arm64Opcode::ARM64_OP_REG &&
         CRegisters::Is64bitReg(op.Operand(0).Reg) && CRegisters::Is64bitReg(op.Operand(1).Reg))
     {
-        if (op.Operand(2).type == Arm64Opcode::ARM64_OP_REG && CRegisters::Is64bitReg(op.Operand(2).Reg))
+        if (op.Operand(2).type == Arm64Opcode::ARM64_OP_IMM)
+        {
+            Reg.Set64(op.Operand(0).Reg, Reg.Get64(op.Operand(1).Reg) + op.Operand(2).ImmVal);
+        }
+        else if (op.Operand(2).type == Arm64Opcode::ARM64_OP_REG && CRegisters::Is64bitReg(op.Operand(2).Reg))
         {
             if (op.Operand(2).shift.type == Arm64Opcode::ARM64_SFT_INVALID && op.Operand(2).Extend == Arm64Opcode::ARM64_EXT_INVALID)
             {
