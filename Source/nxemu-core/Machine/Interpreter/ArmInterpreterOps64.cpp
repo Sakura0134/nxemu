49078c2ffe7fe57b5c9542cd189d3a1346de1944
N3xoX1 <n3xox1@gmail.com>
2020-04-19 17:49:23 +0930

    Add 32bit reg and imm to Arm64Op::Cmp

--- Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
+++ Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
@@ -215,6 +215,11 @@ void Arm64Op::Cmp(CPUExecutor & core, const Arm64Opcode &op)
         a = Reg.Get64(op.Operand(0).Reg);
         b = op.Operand(1).ImmVal;
     }
+    else if (op.Operands() == 2 && op.Operand(0).type == Arm64Opcode::ARM64_OP_REG && CRegisters::Is32bitReg(op.Operand(0).Reg) && op.Operand(1).type == Arm64Opcode::ARM64_OP_IMM)
+    {
+        a = (int64_t)((int32_t)Reg.Get32(op.Operand(0).Reg));
+        b = op.Operand(1).ImmVal;
+    }
     else if (op.Operands() == 2 && op.Operand(0).type == Arm64Opcode::ARM64_OP_REG && CRegisters::Is64bitReg(op.Operand(0).Reg) && op.Operand(1).type == Arm64Opcode::ARM64_OP_REG && CRegisters::Is64bitReg(op.Operand(1).Reg))
     {
         a = Reg.Get64(op.Operand(0).Reg);
