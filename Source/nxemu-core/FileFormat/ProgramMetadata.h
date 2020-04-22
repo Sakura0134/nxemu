fd11501c63fdf0a187a2a3e8bcd122be85c9688d
N3xoX1 <n3xox1@gmail.com>
2020-04-22 10:31:16 +0930

    Tabs to spaces in ProgramMetadata.h

--- Source/nxemu-core/FileFormat/ProgramMetadata.h
+++ Source/nxemu-core/FileFormat/ProgramMetadata.h
@@ -5,33 +5,33 @@
 class CProgramMetadata
 {
 public:
-	CProgramMetadata(uint64_t offset, CEncryptedFile &EncryptedFile, const CPartitionFilesystem::VirtualFile * npdm);
+    CProgramMetadata(uint64_t offset, CEncryptedFile &EncryptedFile, const CPartitionFilesystem::VirtualFile * npdm);
 
-	inline uint8_t GetMainThreadPriority(void) const { return m_Header.main_thread_priority; }
+    inline uint8_t GetMainThreadPriority(void) const { return m_Header.main_thread_priority; }
 
 private:
-	CProgramMetadata(void);                                // Disable default constructor
-	CProgramMetadata(const CProgramMetadata&);             // Disable copy constructor
-	CProgramMetadata& operator=(const CProgramMetadata&);  // Disable assignment
+    CProgramMetadata(void);                                // Disable default constructor
+    CProgramMetadata(const CProgramMetadata&);             // Disable copy constructor
+    CProgramMetadata& operator=(const CProgramMetadata&);  // Disable assignment
 
-	struct Header 
-	{
-		uint8_t magic[4];
-		uint8_t reserved[8];
-		uint8_t flags;
-		uint8_t reserved1;
-		uint8_t main_thread_priority;
-		uint8_t main_thread_cpu;
-		uint8_t reserved2[8];
-		uint32_t process_category;
-		uint32_t main_stack_size;
-		uint8_t application_name[0x10];
-		uint8_t reserved3[0x40];
-		uint32_t aci_offset;
-		uint32_t aci_size;
-		uint32_t acid_offset;
-		uint32_t acid_size;
-	};
+    struct Header
+    {
+        uint8_t magic[4];
+        uint8_t reserved[8];
+        uint8_t flags;
+        uint8_t reserved1;
+        uint8_t main_thread_priority;
+        uint8_t main_thread_cpu;
+        uint8_t reserved2[8];
+        uint32_t process_category;
+        uint32_t main_stack_size;
+        uint8_t application_name[0x10];
+        uint8_t reserved3[0x40];
+        uint32_t aci_offset;
+        uint32_t aci_size;
+        uint32_t acid_offset;
+        uint32_t acid_size;
+    };
 
-	Header m_Header;
+    Header m_Header;
 };
