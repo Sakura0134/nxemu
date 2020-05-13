be01e4e9815f9ac58c765781b05787e6b0b89260
N3xoX1 <n3xox1@gmail.com>
2020-05-14 06:53:00 +0930

    Arm64Op::Add: Add 32bit ARM64_EXT_UXTB

--- Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
+++ Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
@@ -43,6 +43,25 @@ void Arm64Op::Add(CPUExecutor & core, const Arm64Opcode &op)
             g_Notify->BreakPoint(__FILE__, __LINE__);
         }
     }
+    else if (op.Operands() == 3 && op.Operand(0).type == Arm64Opcode::ARM64_OP_REG && op.Operand(1).type == Arm64Opcode::ARM64_OP_REG &&
+        CRegisters::Is32bitReg(op.Operand(0).Reg) && CRegisters::Is32bitReg(op.Operand(1).Reg))
+    {
+        if (op.Operand(2).type == Arm64Opcode::ARM64_OP_REG && CRegisters::Is32bitReg(op.Operand(2).Reg))
+        {
+            if (op.Operand(2).shift.type == Arm64Opcode::ARM64_SFT_INVALID && op.Operand(2).Extend == Arm64Opcode::ARM64_EXT_UXTB)
+            {
+                Reg.Set32(op.Operand(0).Reg, Reg.Get32(op.Operand(1).Reg) + (uint8_t)(Reg.Get32(op.Operand(2).Reg)));
+            }
+            else
+            {
+                g_Notify->BreakPoint(__FILE__, __LINE__);
+            }
+        }
+        else
+        {
+            g_Notify->BreakPoint(__FILE__, __LINE__);
+        }
+    }
     else
     {
         g_Notify->BreakPoint(__FILE__, __LINE__);
