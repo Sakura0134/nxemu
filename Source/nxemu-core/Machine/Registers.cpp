8a145f7603fba7285129f6279b75499dad21ceee
N3xoX1 <n3xox1@gmail.com>
2020-05-23 21:03:13 +0930

    CRegisters::ConditionSet: Add ARM64_CC_LE

--- Source/nxemu-core/Machine/Registers.cpp
+++ Source/nxemu-core/Machine/Registers.cpp
@@ -301,6 +301,7 @@ bool CRegisters::ConditionSet(Arm64Opcode::arm64_cc cc)
     case Arm64Opcode::ARM64_CC_LO: return m_pstate.C == 0;
     case Arm64Opcode::ARM64_CC_HI: return m_pstate.C != 0 && m_pstate.Z == 0;
     case Arm64Opcode::ARM64_CC_LT: return m_pstate.N != m_pstate.V;
+    case Arm64Opcode::ARM64_CC_LE: return m_pstate.Z != 0 || (m_pstate.N != m_pstate.V);
     }
     g_Notify->BreakPoint(__FILE__, __LINE__);
     return false;
