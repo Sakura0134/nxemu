4e0a9b54aca9232d063155308734dbc23eb31d40
N3xoX1 <n3xox1@gmail.com>
2020-06-03 12:05:31 +0930

    CHange thread to be derived from a Kernel Object

--- Source/nxemu-core/hle/Kernel/SystemThread.h
+++ Source/nxemu-core/hle/Kernel/SystemThread.h
@@ -1,13 +1,16 @@
 #pragma once
-#include <map>
 #include <nxemu-core\Machine\CPU\CPUExecutor.h>
 #include <nxemu-core\hle\Memory\SystemThreadMemory.h>
 #include <nxemu-core\hle\Memory\ProcessMemory.h>
+#include <nxemu-core\hle\Kernel\KernelObject.h>
+#include <Common\stdtypes.h>
+#include <map>
 
 class CHleKernel;
 
 class CSystemThread :
-    public CPUExecutor
+    public CPUExecutor,
+    public CKernelObject
 {
 public:
     CSystemThread(CHleKernel * m_Kernel, CProcessMemory &ProcessMemory, const char * name, uint64_t entry_point, uint32_t ThreadHandle, uint32_t thread_id, uint64_t ThreadContext, uint64_t StackTop, uint32_t StackSize, uint32_t Priority, uint32_t ProcessorId);
@@ -17,6 +20,9 @@ private:
     CSystemThread();                               // Disable default constructor
     CSystemThread(const CSystemThread&);            // Disable copy constructor
     CSystemThread& operator=(const CSystemThread&); // Disable assignment
+
+    HandleType GetHandleType() const { return Thread; }
+    CSystemThread * GetSystemThreadPtr(void) { return this; }
 
     void ServiceCall(uint32_t index);
         
