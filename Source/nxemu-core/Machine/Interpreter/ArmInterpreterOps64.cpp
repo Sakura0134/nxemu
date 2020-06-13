236883c67f761ab4b099cf5161d5ae3f67c2918d
N3xoX1 <n3xox1@gmail.com>
2020-06-14 07:53:40 +0930

    Arm64Op::Lsl: Add 64bit

--- Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
+++ Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
@@ -1050,9 +1050,26 @@ void Arm64Op::Lsl(CPUExecutor & core, const Arm64Opcode &op)
 {
     CRegisters & Reg = core.Reg();
 
-    if (op.Operands() == 3 && op.Operand(0).type == Arm64Opcode::ARM64_OP_REG && op.Operand(1).type == Arm64Opcode::ARM64_OP_REG && op.Operand(2).type == Arm64Opcode::ARM64_OP_IMM)
+    if (op.Operands() == 3 && op.Operand(0).type == Arm64Opcode::ARM64_OP_REG && op.Operand(1).type == Arm64Opcode::ARM64_OP_REG && 
+        CRegisters::Is64bitReg(op.Operand(0).Reg) && CRegisters::Is64bitReg(op.Operand(1).Reg))
     {
-        if (CRegisters::Is32bitReg(op.Operand(0).Reg) && CRegisters::Is32bitReg(op.Operand(1).Reg) && op.Operand(2).shift.type == Arm64Opcode::ARM64_SFT_INVALID && op.Operand(2).Extend == Arm64Opcode::ARM64_EXT_INVALID)
+        if (op.Operand(2).type == Arm64Opcode::ARM64_OP_REG && CRegisters::Is64bitReg(op.Operand(2).Reg))
+        {
+            Reg.Set64(op.Operand(0).Reg, Reg.Get64(op.Operand(1).Reg) << Reg.Get64(op.Operand(2).Reg));
+        }
+        else if (op.Operand(2).type == Arm64Opcode::ARM64_OP_IMM)
+        {
+            Reg.Set64(op.Operand(0).Reg, Reg.Get64(op.Operand(1).Reg) << op.Operand(2).ImmVal);
+        }
+        else
+        {
+            g_Notify->BreakPoint(__FILE__, __LINE__);
+        }
+    }
+    else if (op.Operands() == 3 && op.Operand(0).type == Arm64Opcode::ARM64_OP_REG && op.Operand(1).type == Arm64Opcode::ARM64_OP_REG &&
+        CRegisters::Is32bitReg(op.Operand(0).Reg) && CRegisters::Is32bitReg(op.Operand(1).Reg))
+    {
+        if (op.Operand(2).type == Arm64Opcode::ARM64_OP_IMM)
         {
             Reg.Set32(op.Operand(0).Reg, Reg.Get32(op.Operand(1).Reg) << op.Operand(2).ImmVal);
         }
