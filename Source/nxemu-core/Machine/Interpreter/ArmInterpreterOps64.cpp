1ca9a1d11860d0cdc0448a899a92a3213b8f96ac
N3xoX1 <n3xox1@gmail.com>
2021-01-18 16:23:38 +1030

    Arm64Op::Mvn: Handle 32bit reg

--- Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
+++ Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
@@ -2228,6 +2228,11 @@ void Arm64Op::Mvn(CInterpreterCPU & Cpu, const Arm64Opcode & Op)
     {
         Reg.Set64(Op.Operand(0).Reg, ~(Reg.Get64(Op.Operand(1).Reg)));
     }
+    else if (Op.Operands() == 2 && Op.Operand(0).type == Arm64Opcode::ARM64_OP_REG && Op.Operand(1).type == Arm64Opcode::ARM64_OP_REG &&
+        Arm64Opcode::Is32bitReg(Op.Operand(0).Reg) && Arm64Opcode::Is32bitReg(Op.Operand(1).Reg))
+    {
+        Reg.Set32(Op.Operand(0).Reg, ~(Reg.Get32(Op.Operand(1).Reg)));
+    }
     else
     {
         g_Notify->BreakPoint(__FILE__, __LINE__);
