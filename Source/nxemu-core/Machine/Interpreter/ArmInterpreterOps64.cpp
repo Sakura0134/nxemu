ce811cacd2bc563b840cdbe1664431a4804836dd
N3xoX1 <n3xox1@gmail.com>
2021-01-18 21:43:43 +1030

    Arm64Op::Sub: 64bit ARM64_EXT_SXTW

--- Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
+++ Source/nxemu-core/Machine/Interpreter/ArmInterpreterOps64.cpp
@@ -2812,6 +2812,11 @@ void Arm64Op::Sub(CInterpreterCPU & Cpu, const Arm64Opcode & Op)
         {
             b = Reg.Get32(Op.Operand(2).Reg);
         }
+        else if (Op.Operand(2).type == Arm64Opcode::ARM64_OP_REG && Arm64Opcode::Is32bitReg(Op.Operand(2).Reg) &&
+            Op.Operand(2).shift.type == Arm64Opcode::ARM64_SFT_INVALID && Op.Operand(2).Extend == Arm64Opcode::ARM64_EXT_SXTW)
+        {
+            b = (int64_t)((int32_t)Reg.Get32(Op.Operand(2).Reg));
+        }
         else
         {
             g_Notify->BreakPoint(__FILE__, __LINE__);
