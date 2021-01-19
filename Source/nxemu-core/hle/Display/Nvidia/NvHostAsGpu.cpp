67d00f003c7b81b9dd5d1846a2c4d3cb87f063a5
N3xoX1 <n3xox1@gmail.com>
2021-01-19 15:19:39 +1030

    CNvHostAsGpu::GetVaRegions: Add

--- Source/nxemu-core/hle/Display/Nvidia/NvHostAsGpu.cpp
+++ Source/nxemu-core/hle/Display/Nvidia/NvHostAsGpu.cpp
@@ -12,6 +12,9 @@ nvResult CNvHostAsGpu::Ioctl(nvIoctl Ioctl, const CIPCRequest::RequestBuffer& In
     {
         switch (Ioctl.Cmd)
         {
+        case IOCTL_GET_VA_REGIONS:
+            GetVaRegions(InData, OutData);
+            break;
         case IOCTL_INITIALIZE_EX:
             InitializeEx(InData, OutData);
             break;
@@ -24,6 +27,27 @@ nvResult CNvHostAsGpu::Ioctl(nvIoctl Ioctl, const CIPCRequest::RequestBuffer& In
         g_Notify->BreakPoint(__FILE__, __LINE__);
     }
     return nvResult_Success;
+}
+
+void CNvHostAsGpu::GetVaRegions(const std::vector<uint8_t> & InData, std::vector<uint8_t> & OutData)
+{
+    if (InData.size() < sizeof(IoctlGetVaRegions))
+    {
+        g_Notify->BreakPoint(__FILE__, __LINE__);
+        return;
+    }
+    OutData.resize(sizeof(IoctlGetVaRegions));
+    memcpy(OutData.data(), InData.data(), InData.size());
+    IoctlGetVaRegions & params = *((IoctlGetVaRegions *)OutData.data());
+    
+    params.buf_size = 0x30;
+	params.regions[0].Offset = 0x04000000;
+	params.regions[0].PageSize = 0x1000;
+	params.regions[0].Pages = 0x3fbfff;
+
+	params.regions[1].Offset = 0x04000000;
+	params.regions[1].PageSize = 0x10000;
+	params.regions[1].Pages = 0x1bffff;
 }
 
 void CNvHostAsGpu::InitializeEx(const std::vector<uint8_t>& /*InData*/, std::vector<uint8_t>& /*OutData*/)
--- Source/nxemu-core/hle/Display/Nvidia/NvHostAsGpu.h
+++ Source/nxemu-core/hle/Display/Nvidia/NvHostAsGpu.h
@@ -15,6 +15,22 @@ class CNvHostAsGpu :
         IOCTL_INITIALIZE_EX = 0x09,
     };
 
+	struct IoctlVaRegion 
+	{
+		uint64_t Offset;
+		uint32_t PageSize;
+		uint32_t Unknown;
+		uint64_t Pages;
+	};
+
+	struct IoctlGetVaRegions 
+	{
+		uint64_t buf_addr; // (contained output user ptr on linux, ignored)
+		uint32_t buf_size; // forced to 2*sizeof(struct va_region)
+		uint32_t reserved;
+		IoctlVaRegion regions[2];
+	};
+
 public:
     CNvHostAsGpu(CNvDriver& NvDriver);
 
@@ -25,6 +41,7 @@ private:
     CNvHostAsGpu(const CNvHostAsGpu&);
     CNvHostAsGpu& operator=(const CNvHostAsGpu&);
 
+    void GetVaRegions(const std::vector<uint8_t> & InData, std::vector<uint8_t> & OutData);
     void InitializeEx(const std::vector<uint8_t> & InData, std::vector<uint8_t> & OutData);
     
     CNvDriver & m_NvDriver;
