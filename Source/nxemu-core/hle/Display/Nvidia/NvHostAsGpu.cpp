5ba720d08cb582db015d713963feff35cf558bdd
N3xoX1 <n3xox1@gmail.com>
2021-01-19 15:10:56 +1030

    CNvHostAsGpu::InitializeEx: Add

--- Source/nxemu-core/hle/Display/Nvidia/NvHostAsGpu.cpp
+++ Source/nxemu-core/hle/Display/Nvidia/NvHostAsGpu.cpp
@@ -8,6 +8,25 @@ CNvHostAsGpu::CNvHostAsGpu(CNvDriver& NvDriver) :
 
 nvResult CNvHostAsGpu::Ioctl(nvIoctl Ioctl, const CIPCRequest::RequestBuffer& InData, CIPCRequest::RequestBuffer& OutData)
 {
-    g_Notify->BreakPoint(__FILE__, __LINE__);
+    if (Ioctl.Group == nvIoctl::NvGpuAsMagic)
+    {
+        switch (Ioctl.Cmd)
+        {
+        case IOCTL_INITIALIZE_EX:
+            InitializeEx(InData, OutData);
+            break;
+        default:
+            g_Notify->BreakPoint(__FILE__, __LINE__);
+        }
+    }
+    else
+    {
+        g_Notify->BreakPoint(__FILE__, __LINE__);
+    }
     return nvResult_Success;
+}
+
+void CNvHostAsGpu::InitializeEx(const std::vector<uint8_t>& /*InData*/, std::vector<uint8_t>& /*OutData*/)
+{
+    //stubbed
 }
--- Source/nxemu-core/hle/Display/Nvidia/NvHostAsGpu.h
+++ Source/nxemu-core/hle/Display/Nvidia/NvHostAsGpu.h
@@ -6,6 +6,15 @@ class CNvDriver;
 class CNvHostAsGpu :
     public INvDevice
 {
+    enum
+    {
+        IOCTL_BIND_CHANNEL = 0x01,
+        IOCTL_ALLOC_SPACE = 0x02,
+        IOCTL_MAP_BUFFER_EX = 0x06,
+        IOCTL_GET_VA_REGIONS = 0x08,
+        IOCTL_INITIALIZE_EX = 0x09,
+    };
+
 public:
     CNvHostAsGpu(CNvDriver& NvDriver);
 
@@ -16,5 +25,7 @@ private:
     CNvHostAsGpu(const CNvHostAsGpu&);
     CNvHostAsGpu& operator=(const CNvHostAsGpu&);
 
+    void InitializeEx(const std::vector<uint8_t> & InData, std::vector<uint8_t> & OutData);
+    
     CNvDriver & m_NvDriver;
 };
