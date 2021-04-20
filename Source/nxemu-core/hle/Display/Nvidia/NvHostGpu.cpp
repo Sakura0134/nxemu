51631dab57f1ae9df106e17cd8d0cb8f3fdd6da8
N3xoX1 <n3xox1@gmail.com>
2021-04-20 17:17:56 +0930

    CNvHostGpu: Handle CHANNEL_SUBMIT_GPFIFO

--- Source/nxemu-core/hle/Display/Nvidia/NvHostGpu.cpp
+++ Source/nxemu-core/hle/Display/Nvidia/NvHostGpu.cpp
@@ -1,4 +1,5 @@
 #include <nxemu-core\hle\Display\Nvidia\NvHostGpu.h>
+#include <nxemu-core\hle\Display\Nvidia\NvDriver.h>
 #include <nxemu-core\SystemGlobals.h>
 
 CNvHostGpu::CNvHostGpu(CNvDriver & NvDriver) :
@@ -25,6 +26,9 @@ nvResult CNvHostGpu::Ioctl(nvIoctl Ioctl, const CIPCRequest::RequestBuffer & InD
         {
         case CHANNEL_SET_NVMAP_FD:
             SetNvMap(InData, OutData);
+            break;
+        case CHANNEL_SUBMIT_GPFIFO:
+            SubmitGpfifo(InData, OutData);
             break;
         case CHANNEL_ALLOC_OBJ_CTX:
             AllocObjCtx(InData, OutData);
@@ -80,6 +84,29 @@ void CNvHostGpu::SetUserData(const CIPCRequest::RequestBuffer & /*InData*/, CIPC
 void CNvHostGpu::SetPriority(const CIPCRequest::RequestBuffer& /*InData*/, CIPCRequest::RequestBuffer& /*OutData*/)
 {
     //Stubbed
+}
+
+void CNvHostGpu::SubmitGpfifo(const CIPCRequest::RequestBuffer & InData, CIPCRequest::RequestBuffer & OutData)
+{
+    if (InData.size() < sizeof(IoctlSubmitGpfifo))
+    {
+        g_Notify->BreakPoint(__FILE__, __LINE__);
+        return;
+    }
+    IoctlSubmitGpfifo Params;
+    memcpy(&Params, InData.data(), sizeof(IoctlSubmitGpfifo));
+
+    if (InData.size() != sizeof(IoctlSubmitGpfifo) + Params.NumEntries * sizeof(uint64_t))
+    {
+        g_Notify->BreakPoint(__FILE__, __LINE__);
+        return;
+    }
+    uint32_t SyncPointValue = m_NvDriver.Video().GetSyncPointValue(Params.FenceOut.id);
+    Params.FenceOut.value = Params.Flags.Increment != 0 ? Params.FenceOut.value + SyncPointValue : SyncPointValue;
+
+    m_NvDriver.Video().PushGPUEntries((const uint64_t *)&InData[sizeof(IoctlSubmitGpfifo)], Params.NumEntries);
+    OutData.resize(sizeof(Params));
+    std::memcpy(OutData.data(), &Params, sizeof(Params));
 }
 
 void CNvHostGpu::ZcullBind(const CIPCRequest::RequestBuffer & /*InData*/, CIPCRequest::RequestBuffer & /*OutData*/)
--- Source/nxemu-core/hle/Display/Nvidia/NvHostGpu.h
+++ Source/nxemu-core/hle/Display/Nvidia/NvHostGpu.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <nxemu-core\hle\Display\Nvidia\NvDevice.h>
+#include <nxemu-core\hle\Display\Nvidia\NvMultiFence.h>
 
 class CNvDriver;
 
@@ -9,12 +10,35 @@ class CNvHostGpu :
     enum 
     {
         CHANNEL_SET_NVMAP_FD = 0x01,
+        CHANNEL_SUBMIT_GPFIFO = 0x08,
         CHANNEL_ALLOC_OBJ_CTX = 0x09,
         CHANNEL_ZCULL_BIND = 0x0B,
         CHANNEL_SET_ERROR_NOTIFIER = 0x0C,
         CHANNEL_SET_PRIORITY = 0x0D,
         CHANNEL_SET_USER_DATA = 0x14,
         CHANNEL_ALLOC_GPFIFO_EX2 = 0x1A,
+    };
+
+    struct IoctlSubmitGpfifo 
+    {
+        uint64_t Gpfifo;
+        uint32_t NumEntries;
+#pragma warning(push)
+#pragma warning(disable : 4201) // nonstandard extension used : nameless struct/union
+        union 
+        {
+            uint32_t raw;
+            struct
+            {
+                unsigned AddWait : 1;
+                unsigned AddIncrement : 1;
+                unsigned NewHWFormat : 1;
+                unsigned  : 5;
+                unsigned Increment : 1;
+            } ;
+        } Flags;
+#pragma warning(pop)
+        NvFence FenceOut;
     };
 
 public:
@@ -33,6 +57,7 @@ private:
     void SetErrorNotifier(const CIPCRequest::RequestBuffer & InData, CIPCRequest::RequestBuffer & OutData);
     void SetUserData(const CIPCRequest::RequestBuffer & InData, CIPCRequest::RequestBuffer & OutData);
     void SetPriority(const CIPCRequest::RequestBuffer & InData, CIPCRequest::RequestBuffer & OutData);
+    void SubmitGpfifo(const CIPCRequest::RequestBuffer & InData, CIPCRequest::RequestBuffer & OutData);
     void ZcullBind(const CIPCRequest::RequestBuffer & InData, CIPCRequest::RequestBuffer & OutData);
 
     CNvDriver & m_NvDriver;
